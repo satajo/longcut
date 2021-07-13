82a2502027ea0e9793be1686b3903b244b83e3a3
Sami Jokela <sami.jokela@satajo.com>
2021-07-13 12:50:32 +0300

    Handle layer actions in a transition function

--- ordinator-core/src/model/state_machine/mod.rs
+++ ordinator-core/src/model/state_machine/mod.rs
@@ -1,14 +1,13 @@
 mod state;
-mod transition;
 mod view;
 
 use crate::model::key::KeyPress;
-use crate::model::layer::{Action, Layer};
-use crate::model::state_machine::state::{Branch, Finished, GlobalKeys, Inactive, Root, State};
-use crate::model::state_machine::transition::{
-    TransitionBranch, TransitionCancel, TransitionExecute, TransitionReset, TransitionStart,
-    TransitionUnbranch, UnbranchResult,
+use crate::model::layer::Layer;
+use crate::model::state_machine::state::{
+    Branch, Finished, GlobalKeys, Inactive, LayerActionResult, Root, State, TransitionCancel,
+    TransitionLayerAction, TransitionReset, TransitionStart, TransitionUnbranch, UnbranchResult,
 };
+
 use crate::model::state_machine::view::Viewable;
 use crate::port::input::Input;
 use crate::port::view::View;
@@ -53,35 +52,26 @@ pub trait FsmState {
 impl FsmState for State<Inactive> {
     fn step(self, input: &impl Input, view: &impl View) -> Fsm {
         view.render(&self.to_view_data());
-
-        // Pressed key does not matter since we know it is one of the start keys.
         input.capture_one(&self.keys.start);
 
-        let state = self.start();
-        Fsm::Root(state)
+        // Pressed key does not matter since we know it is one of the start keys.
+        Fsm::Root(self.start())
     }
 }
 
 impl FsmState for State<Root> {
     fn step(self, input: &impl Input, view: &impl View) -> Fsm {
         view.render(&self.to_view_data());
-
         let press = input.capture_any();
 
-        // Global keys
         if self.keys.cancel.contains(&press) {
             Fsm::Inactive(self.cancel())
-
-        // Layer actions
-        } else if let Some(action) = self.root.actions.get(&press) {
-            match action.clone() {
-                Action::Branch(layer) => Fsm::Branch(self.branch(layer)),
-                Action::Command() => Fsm::Inactive(self.execute()),
-            }
-
-        // No match
         } else {
-            Fsm::Root(self)
+            match self.layer_action(&press) {
+                LayerActionResult::Branched(state) => Fsm::Branch(state),
+                LayerActionResult::Executed(state) => Fsm::Inactive(state),
+                LayerActionResult::NotFound(state) => Fsm::Root(state),
+            }
         }
     }
 }
@@ -89,10 +79,8 @@ impl FsmState for State<Root> {
 impl FsmState for State<Branch> {
     fn step(self, input: &impl Input, view: &impl View) -> Fsm {
         view.render(&self.to_view_data());
-
         let press = input.capture_any();
 
-        // Global keys
         if self.keys.cancel.contains(&press) {
             Fsm::Inactive(self.cancel())
         } else if self.keys.reset.contains(&press) {
@@ -102,17 +90,12 @@ impl FsmState for State<Branch> {
                 UnbranchResult::Branch(state) => Fsm::Branch(state),
                 UnbranchResult::Root(state) => Fsm::Root(state),
             }
-
-        // Layer actions
-        } else if let Some(action) = self.state.branches.last().unwrap().actions.get(&press) {
-            match action.clone() {
-                Action::Branch(layer) => Fsm::Branch(self.branch(layer)),
-                Action::Command() => Fsm::Inactive(self.execute()),
-            }
-
-        // No match
         } else {
-            Fsm::Branch(self)
+            match self.layer_action(&press) {
+                LayerActionResult::Branched(state) => Fsm::Branch(state),
+                LayerActionResult::Executed(state) => Fsm::Inactive(state),
+                LayerActionResult::NotFound(state) => Fsm::Branch(state),
+            }
         }
     }
 }
--- ordinator-core/src/model/state_machine/state.rs
+++ ordinator-core/src/model/state_machine/state.rs
@@ -1,5 +1,5 @@
 use crate::model::key::KeyPress;
-use crate::model::layer::Layer;
+use crate::model::layer::{Action, Layer};
 
 pub struct GlobalKeys {
     pub cancel: Vec<KeyPress>,
@@ -9,7 +9,7 @@ pub struct GlobalKeys {
 }
 
 //-----------------------------------------------------------------------------
-// Common state structure
+// States
 //-----------------------------------------------------------------------------
 
 pub struct State<S> {
@@ -19,7 +19,7 @@ pub struct State<S> {
 }
 
 impl<S> State<S> {
-    pub fn swap_state<S2>(self, state: S2) -> State<S2> {
+    fn swap_state<S2>(self, state: S2) -> State<S2> {
         State {
             root: self.root,
             keys: self.keys,
@@ -27,10 +27,6 @@ impl<S> State<S> {
         }
     }
 }
-
-//-----------------------------------------------------------------------------
-// State declaration
-//-----------------------------------------------------------------------------
 
 pub struct Branch {
     // todo: Use non-empty list
@@ -42,3 +38,178 @@ pub struct Finished;
 pub struct Inactive;
 
 pub struct Root;
+
+//-----------------------------------------------------------------------------
+// Transitions
+//-----------------------------------------------------------------------------
+
+pub trait TransitionBranch {
+    fn branch(self, layer: Layer) -> State<Branch>;
+}
+
+impl TransitionBranch for State<Branch> {
+    fn branch(mut self, layer: Layer) -> State<Branch> {
+        println!("Branch! Branch");
+        self.state.branches.push(layer);
+        self
+    }
+}
+
+impl TransitionBranch for State<Root> {
+    fn branch(self, layer: Layer) -> State<Branch> {
+        println!("Branch! Branch");
+        self.swap_state(Branch {
+            branches: vec![layer],
+        })
+    }
+}
+
+// Cancel
+
+pub trait TransitionCancel {
+    fn cancel(self) -> State<Inactive>;
+}
+
+impl TransitionCancel for State<Branch> {
+    fn cancel(self) -> State<Inactive> {
+        println!("Cancel! Inactive");
+        self.swap_state(Inactive)
+    }
+}
+
+impl TransitionCancel for State<Root> {
+    fn cancel(self) -> State<Inactive> {
+        println!("Cancel! Inactive");
+        self.swap_state(Inactive)
+    }
+}
+
+// Execute
+
+pub trait TransitionExecute {
+    fn execute(self) -> State<Inactive>;
+}
+
+impl TransitionExecute for State<Branch> {
+    fn execute(self) -> State<Inactive> {
+        println!("Execute! Inactive");
+        self.swap_state(Inactive)
+    }
+}
+
+impl TransitionExecute for State<Root> {
+    fn execute(self) -> State<Inactive> {
+        println!("Execute! Inactive");
+        self.swap_state(Inactive)
+    }
+}
+
+// Exit
+
+pub trait TransitionExit {
+    fn exit(self) -> State<Finished>;
+}
+
+impl TransitionExit for State<Branch> {
+    fn exit(self) -> State<Finished> {
+        println!("Exit! Finished");
+        self.swap_state(Finished)
+    }
+}
+
+impl TransitionExit for State<Root> {
+    fn exit(self) -> State<Finished> {
+        println!("Exit! Finished");
+        self.swap_state(Finished)
+    }
+}
+
+// Layer action
+
+pub enum LayerActionResult<S> {
+    Branched(State<Branch>),
+    Executed(State<Inactive>),
+    NotFound(S),
+}
+
+pub trait TransitionLayerAction {
+    fn layer_action(self, press: &KeyPress) -> LayerActionResult<Self>
+    where
+        Self: Sized;
+}
+
+impl TransitionLayerAction for State<Branch> {
+    fn layer_action(self, press: &KeyPress) -> LayerActionResult<Self> {
+        if let Some(action) = self.state.branches.last().unwrap().actions.get(&press) {
+            match action.clone() {
+                Action::Branch(layer) => LayerActionResult::Branched(self.branch(layer)),
+                Action::Command() => LayerActionResult::Executed(self.execute()),
+            }
+        } else {
+            LayerActionResult::NotFound(self)
+        }
+    }
+}
+
+impl TransitionLayerAction for State<Root> {
+    fn layer_action(self, press: &KeyPress) -> LayerActionResult<Self> {
+        if let Some(action) = self.root.actions.get(&press) {
+            match action.clone() {
+                Action::Branch(layer) => LayerActionResult::Branched(self.branch(layer)),
+                Action::Command() => LayerActionResult::Executed(self.execute()),
+            }
+        } else {
+            LayerActionResult::NotFound(self)
+        }
+    }
+}
+
+// Reset
+
+pub trait TransitionReset {
+    fn reset(self) -> State<Root>;
+}
+
+impl TransitionReset for State<Branch> {
+    fn reset(self) -> State<Root> {
+        println!("Reset! Root");
+        self.swap_state(Root)
+    }
+}
+
+// Start
+
+pub trait TransitionStart {
+    fn start(self) -> State<Root>;
+}
+
+impl TransitionStart for State<Inactive> {
+    fn start(self) -> State<Root> {
+        println!("Start! Root");
+        self.swap_state(Root)
+    }
+}
+
+// Unbranch
+
+pub enum UnbranchResult {
+    Branch(State<Branch>),
+    Root(State<Root>),
+}
+
+pub trait TransitionUnbranch {
+    fn unbranch(self) -> UnbranchResult;
+}
+
+impl TransitionUnbranch for State<Branch> {
+    fn unbranch(mut self) -> UnbranchResult {
+        self.state.branches.pop();
+        if self.state.branches.is_empty() {
+            println!("Unbranch! Root");
+            UnbranchResult::Root(self.swap_state(Root))
+        } else {
+            println!("Unbranch! Branch");
+            UnbranchResult::Branch(self)
+        }
+    }
+}

71daeb39a4dcc7d7c1956c87259b92e5a6ac87c5
Sami Jokela <sami.jokela@satajo.com>
2022-01-04 20:32:57 +0200

    Pass configuration file path as parameter

--- ordinator-linux/src/main.rs
+++ ordinator-linux/src/main.rs
@@ -6,10 +6,20 @@ mod x11;
 use crate::gdk::GdkApplication;
 use crate::system::ShellExecutor;
 use crate::x11::X11;
+use clap::Parser;
 use ordinator_core::run;
+use std::path::Path;
+
+#[derive(Parser)]
+struct Args {
+    #[clap(short, long)]
+    config: String,
+}
 
 fn main() {
-    let path = "ordinator.yaml".as_ref();
+    let args = Args::parse();
+
+    let path = Path::new(&args.config);
     let configuration = match config::read_config(path) {
         Ok(config) => config,
         Err(err) => {

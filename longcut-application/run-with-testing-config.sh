8188b299bb5cae92e024dc75297aaec2b17e1cb8
Sami Jokela <sami.jokela@satajo.com>
2023-06-22 12:00:12 +0300

    Add testing config helper script
    There is some nuance in running the testing configuration in a safe way. This
    script should both guarantee that timeout is used to wrap the execution,
    preventing lock-ups, and making sure that the timeout is in fact constant.

---
+++ longcut-application/run-with-testing-config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e -o pipefail
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+TIMEOUT="15s"
+
+# A separate build step is needed before the timeout to prevent the build time from cannibalizing the run time.
+cargo build
+
+# Instead of using "cargo run", the built binary is executed directly. This avoids an implicit rebuild,
+# where cargo would re-output build warnings etc, cluttering up the output.
+echo "Running with a timeout of ${TIMEOUT}"
+exec timeout $TIMEOUT ../target/debug/longcut --config testing-config.yaml

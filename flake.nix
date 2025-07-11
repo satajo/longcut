35500be3edcec2f0219c0f8eb73cc76d5a998351
Sami Jokela <sami.jokela@satajo.com>
2025-07-11 21:02:26 +0200

    Add proper dev shell to the flake

--- flake.nix
+++ flake.nix
@@ -16,12 +16,22 @@
         src = nixpkgs.lib.cleanSource ./.;
         cargoLock = { lockFile = ./Cargo.lock; };
 
-        nativeBuildInputs = with pkgs; [ pkg-config ];
         buildInputs = with pkgs; [ glib xorg.libX11 gtk3 ];
+        nativeBuildInputs = with pkgs; [ pkg-config ];
 
         checkFlags = [
           # longcut_x11: Test fails with a "signal: 11, SIGSEGV: invalid memory reference"
           "--skip=handle::tests::test_string_to_keycode"
+        ];
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
+        inputsFrom = [ self.packages.${system}.default ];
+        nativeBuildInputs = with pkgs; [
+          cargo-watch
+          clippy
+          rustfmt
+          rust-analyzer
         ];
       };
     };
